3cb67675ee14bf63141506b295c1545eb12a7259
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-27 17:00:22 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,57 +1,64 @@
 import java.util.Scanner;
 
+/*Assunto: Fila e InsertionSort
+ *Grupo:Carlos Pereira
+ * 	Heber Ribeiro
+ * 	Henrique Lima
+ * 	Ricardo Lima
+ * 	Thiago Albertins*/
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		/*Condicao do While*/
+
+		/* Condicao do While */
 		boolean continua = true;
+		
 		Scanner scanner = new Scanner(System.in);
 
 		do {
 			/* Menu */
 			System.out.println("\nSelecione uma opcao: \n" + "1 - Senha Normal \n" + "2 - Senha Preferencial \n"
-					+ "3 - Atendimento \n" + "4 - Listar senhas \n" + "0 - Finalizar Programa");
+					+ "3 - Atendimento \n" + "4 - Listar Senhas \n" + "0 - Finalizar Programa");
 			try {
-				/*Leitor da Opcao do Usuario*/
+				/* Leitor da Opcao do Usuario */
 				int menu = scanner.nextInt();
 				switch (menu) {
-				
-				/*Pedir Senha Normal*/
+
+				/* Pedir Senha Normal */
 				case 1:
 					Fila.inserir(Gerador.senhaNormal());
 					break;
-					
-					/*Pedir Senha Preferencial*/
+
+				/* Pedir Senha Preferencial */
 				case 2:
 					Fila.inserir(Gerador.senhaPreferencial());
 					Fila.insertionSort();
 					break;
-					
-					/*Chamar Senha*/
+
+				/* Chamar Senha */
 				case 3:
-					System.out.println("Proximo da fila!");
 					Fila.remover();
 					break;
-					
-					/*Listar Senhas*/
+
+				/* Listar Senhas */
 				case 4:
 					Fila.listar();
 					break;
-					
-					/*Finalizar Programa*/
+
+				/* Finalizar Programa */
 				case 0:
 					System.out.println("Programa finalizado!");
 					continua = false;
 					break;
-					
-					/*Opcao Errada*/
+
+				/* Opcao Errada */
 				default:
 					System.out.println("Opcao incorreta!\n");
 					break;
 				}
-				
-				/*Excecao*/
+
+				/* Excecao */
 			} catch (Exception e) {
 				System.out.println("Opcao incorreta!\n");
 				scanner.next();
